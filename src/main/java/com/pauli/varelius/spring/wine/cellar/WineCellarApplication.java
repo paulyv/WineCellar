811892a8acb631646a04bdab81237123bcb4930f
Pauli Varelius <pauli.varelius@gmail.com>
2017-01-06 18:13:13 +0200

    initial commit

---
+++ src/main/java/com/pauli/varelius/spring/wine/cellar/WineCellarApplication.java
@@ -0,0 +1,12 @@
+package com.pauli.varelius.spring.wine.cellar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineCellarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineCellarApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pauli/varelius/spring/wine/cellar/WineCellarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pauli.varelius.spring.wine.cellar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WineCellarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
